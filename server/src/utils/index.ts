2e16ffd342f0f7c3ae3e7ba1f7f8b6e60a8fced0
Yevhenii Orhanistyi <organistiy@gmail.com>
2026-03-25 14:26:58 +0100

    feat: add random code generator utility

---
+++ server/src/utils/index.ts
@@ -0,0 +1,9 @@
+export const generateCode = (): string => {
+  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let code = "";
+  for (let i = 0; i < 6; i++) {
+    code += chars[Math.floor(Math.random() * chars.length)];
+  }
+
+  return code;
+};

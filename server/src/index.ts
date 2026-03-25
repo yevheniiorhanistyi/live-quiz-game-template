a2b0432f959c09359d436d2e92908f939a242511
Yevhenii Orhanistyi <organistiy@gmail.com>
2026-03-25 14:45:09 +0100

    feat: implement websocket server with message routing logic

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,9 @@
-import { WebSocketServer } from 'ws';
-
+import { WebSocketServer } from "ws";
+import { setupWebSocket } from "./ws";
 
 const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 
 // WebSocket server
 const wss = new WebSocketServer({ port: PORT });
+
+setupWebSocket(wss);

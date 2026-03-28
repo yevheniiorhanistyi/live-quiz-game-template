6bd5d8e99bd49d7b349521c768aea2e1c753819a
Yevhenii Orhanistyi <organistiy@gmail.com>
2026-03-28 10:30:50 +0100

    feat: implement point calculation and scoreboard formatting

--- server/src/utils/index.ts
+++
@@ -1,9 +0,0 @@
-export const generateCode = (): string => {
-  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-  let code = "";
-  for (let i = 0; i < 6; i++) {
-    code += chars[Math.floor(Math.random() * chars.length)];
-  }
-
-  return code;
-};
